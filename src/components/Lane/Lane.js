51f5cc85b89d67eadd029223cff855648ac61a03
kumarabhishekbansal <kumarabhishekbansal>
2024-03-27 10:41:15 +0530

    used styled-components

--- src/components/Lane/Lane.js
+++ src/components/Lane/Lane.js
@@ -1,9 +1,41 @@
-import "./Lane.css";
+import styled from "styled-components";
 import Task from "../Task/Task";
-function Lane({ laneId,title, loading, error, tasks,onDragStart,onDragOver,onDrop }) {
+
+const LaneWrapper = styled.div`
+  text-align: left;
+  padding: 0;
+  background: lightGray;
+  border-radius: 20px;
+  min-height: 50vh;
+  width: 20vw;
+  @media (max-width: 768px) {
+    margin-bottom: 5%;
+  }
+`;
+
+const Title = styled.h2`
+  width: 100%;
+  padding-bottom: 10px;
+  text-align: center;
+  border-bottom: 1px solid darkGray;
+`;
+
+function Lane({
+  laneId,
+  title,
+  loading,
+  error,
+  tasks,
+  onDragStart,
+  onDragOver,
+  onDrop,
+}) {
   return (
-    <div className="Lane-wrapper" onDragOver={onDragOver} onDrop={(e) => onDrop(e, laneId)}>
-      <h2>{title}</h2>
+    <LaneWrapper
+      onDragOver={onDragOver}
+      onDrop={(e) => onDrop(e, laneId)}
+    >
+      <Title>{title}</Title>
       {loading || error ? (
         <span>{error || "Loading..."}</span>
       ) : (
@@ -17,7 +49,7 @@ function Lane({ laneId,title, loading, error, tasks,onDragStart,onDragOver,onDro
           />
         ))
       )}
-    </div>
+    </LaneWrapper>
   );
 }
 
