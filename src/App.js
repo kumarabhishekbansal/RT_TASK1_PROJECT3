55b046414f58c15adaf33fcaf4d5f56a399e448c
kumarabhishekbansal <kumarabhishekbansal>
2024-03-27 10:08:09 +0530

    created backlog with custom react hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Board from './pages/board/Board';
 import Header from './components/Header/Header';
-
+import Backlog from './pages/Backlog/Backlog';
 function App() {
   return (
     <div className='App'>
       <Header />
       <Board />
+      <Backlog />
     </div>
   );
 }
--- src/pages/board/Board.js
+++ src/pages/board/Board.js
@@ -1,7 +1,6 @@
 import Lane from "../../components/Lane/Lane";
-import { useState, useEffect } from "react";
 import "./Board.css";
-
+import useDataFetching from "../../hooks/useDataFetching";
 const lanes = [
   { id: 1, title: "To Do" },
   { id: 2, title: "In Progress" },
@@ -10,27 +9,9 @@ const lanes = [
 ];
 
 function Board() {
-  const [loading, setLoading] = useState(false);
-  const [tasks, setTasks] = useState([]);
-  const [error, setError] = useState("");
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const tasks = await fetch(
-          `https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/tasks`
-        );
-        const result = await tasks.json();
-        if (result) {
-          setTasks(result);
-          setLoading(false);
-        }
-      } catch (e) {
-        setLoading(false);
-        setError(e.message);
-      }
-    }
-    fetchData();
-  }, []);
+  const [loading, error, tasks] = useDataFetching(
+    `https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/tasks`
+  );
   return (
     <div className="Board-wrapper">
       {lanes.map((lane) => (
